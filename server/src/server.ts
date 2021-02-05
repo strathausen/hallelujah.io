a3e85128da4e6253af5edccce96d4ad98200e7d3
Johann Philipp Strathausen <strathausen@gmail.com>
2021-02-05 14:51:36 +0100

    public dir serving

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,15 +1,19 @@
 import Koa from 'koa'
 import Router from 'koa-router'
 import logger from 'koa-logger'
+import Static from 'koa-static'
 import searchRouter from './routes/search'
 import contentRouter from './routes/content'
 
 const app = new Koa()
 const router = new Router()
+const root = 'public'
 
 router.use('/api/search', searchRouter.routes())
 
 router.use(contentRouter.routes())
+
+router.use(Static(root))
 
 const host = process.env.HOST || '127.0.0.1'
 const port = process.env.PORT || 3000
