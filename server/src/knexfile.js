b2a51a03f9b8935074401e88efa9906a7161a808
Johann Philipp Strathausen <strathausen@gmail.com>
2021-02-05 14:45:26 +0100

    new knesfile just for server

---
+++ server/src/knexfile.js
@@ -0,0 +1,17 @@
+module.exports = {
+
+  development: {
+    client: 'postgresql',
+    connection: process.env.DATABASE_URL,
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: process.env.DATABASE_URL,
+    pool: {
+      min: 2,
+      max: 10,
+    },
+  },
+
+}
